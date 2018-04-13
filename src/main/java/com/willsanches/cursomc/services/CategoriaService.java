a64b47f756ac29af7c1f9688bfd235b3fc59c48a
William Sanches <willsancheslima@gmail.com>
2018-04-13 05:51:46 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/willsanches/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.willsanches.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.willsanches.cursomc.domain.Categoria;
+import com.willsanches.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+}
